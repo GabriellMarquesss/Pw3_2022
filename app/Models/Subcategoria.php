e5def7cac879d1e9958f30da62c951c5443ce02f
GabriellMarquesss <marquesgabriel768@gmail.com>
2022-09-08 22:04:57 -0300

    Aula dia 08/09/2022

--- app/Models/Subcategoria.php
+++ app/Models/Subcategoria.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Subcategoria extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'nome',
+        'categoria_id'
+    ];
 }
+
