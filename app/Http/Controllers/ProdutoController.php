1fc87d5e1cfb0a3edc3a80089a049397d3c498e7
GabriellMarquesss <marquesgabriel768@gmail.com>
2022-09-22 22:09:22 -0300

    Aula dia 22/09/2022 (não está funcionando)

---
+++ app/Http/Controllers/ProdutoController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Produto;
+use App\Models\Subcategoria;
+use Illuminate\Http\Request;
+
+class ProdutoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $produtos = Produto::all();
+        return view('admin.produtos.index', compact('produtos'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $subcategorias
+        return view('admin.produtos.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Produto::create($request->all());
+        return redirect()->route('produtos.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Produto  $produto
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Produto $produto)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Produto  $produto
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Produto $produto)
+    {
+        $subcategorias = Subcategoria::all();
+        return view('admin.produtos.edit', compact('subcategorias'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Produto  $produto
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Produto $produto)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Produto  $produto
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Produto $produto)
+    {
+        $produto->delete();
+        return redirect()->route('produtos.index');
+    }
+}
